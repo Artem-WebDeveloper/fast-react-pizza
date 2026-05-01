af895e7ae1940a55ac83f84bcd79b7900d5d44ee
Artem-WebDeveloper <ave_art@icloud.com>
2026-05-01 19:56:26 +0300

    fix: types

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,11 +1,11 @@
-export function formatCurrency(value) {
+export function formatCurrency(value: number) {
   return new Intl.NumberFormat('en', {
     style: 'currency',
     currency: 'EUR',
   }).format(value);
 }
 
-export function formatDate(dateStr) {
+export function formatDate(dateStr: string) {
   return new Intl.DateTimeFormat('en', {
     day: 'numeric',
     month: 'short',
@@ -14,7 +14,7 @@ export function formatDate(dateStr) {
   }).format(new Date(dateStr));
 }
 
-export function calcMinutesLeft(dateStr) {
+export function calcMinutesLeft(dateStr: string) {
   const d1 = new Date().getTime();
   const d2 = new Date(dateStr).getTime();
   return Math.round((d2 - d1) / 60000);
@@ -22,4 +22,6 @@ export function calcMinutesLeft(dateStr) {
 
 // https://uibakery.io/regex-library/phone-number
 export const isValidPhone = (str: string) =>
-  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(str);
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+    str,
+  );
